24319f8386fec535b772e243509bc5cc5c0fb8e1
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-01-25 15:03:23 -0300

    Product

---
+++ src/main/java/com/saretto/course/repositories/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.saretto.course.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.saretto.course.entities.Category;
+import com.saretto.course.entities.Product;
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
