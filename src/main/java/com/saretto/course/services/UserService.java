1cdc0725e0ab4d7c424ee17c9430b8998f5aec6e
GabrielSaretto <gabriel.saretto2@gmail.com>
2024-01-27 18:51:40 -0300

    Exception handling - findById

--- src/main/java/com/saretto/course/services/UserService.java
+++ src/main/java/com/saretto/course/services/UserService.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Service;
 
 import com.saretto.course.entities.User;
 import com.saretto.course.repositories.UserRepository;
+import com.saretto.course.services.exceptions.ResourceNotFoundException;
 
 @Service
 public class UserService {
@@ -21,7 +22,7 @@ public class UserService {
 
 	public User findById(Long id) {
 		Optional<User> obj = repository.findById(id);
-		return obj.get();
+		return obj.orElseThrow(() -> new ResourceNotFoundException(id));
 	}
 	
 	public User insert(User obj) {
@@ -41,6 +42,6 @@ public class UserService {
 	private void updateData(User entity, User obj) {
 		entity.setName(obj.getName());
 		entity.setEmail(obj.getEmail());
-		entity.setPhone(obj.getPhone());	
+		entity.setPhone(obj.getPhone());
 	}
 }
